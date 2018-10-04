8b24e6d679caa6c6188058a3aefa5df5c3d3dfc6
bruno bisogni <bruno@virtuallight.com.br>
2018-10-04 03:28:08 -0300

    ate aula 5

---
+++ maior_ou_menor.rb
@@ -0,0 +1,30 @@
+puts "Bem vindo ao jogo da Adivinhação"
+
+puts "Qual é o seu nome?"
+
+
+nome = gets
+
+puts
+puts
+puts
+puts
+puts
+puts
+puts "Começaremos o jogo para você" + nome 
+
+puts "Escolhendo um número secreto entre 0 e 200.."
+
+numero_secreto = 175
+puts "Que tal adivinhar hoje nosso número secreto?"
+
+puts
+puts
+puts
+puts
+puts
+puts
+
+puts "Tentativa 1..."
+puts "Entre com o número"
+chute = gets
